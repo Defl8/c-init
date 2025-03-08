8ea7ac4266ab190bfd277f1d2d57f2e6163abe3e
Wyatt James <wyattsamueljames@gmail.com>
2025-03-08 02:49:01 +0000

    chore: add clang format

--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) { (void)printf("Hello, World\r\n"); }
+// argc: Amount of args passed from the command line
+int main(int argc, char *argv[]) {
+	char *userInput = *(++argv);
+	if (userInput == NULL) {
+		(void)printf("You didn't enter shit you dumbass\r\n");
+		exit(EXIT_FAILURE); // No dir is entered as an arg
+	} else {
+		(void)printf("The arg entered was: %s\r\n", userInput);
+	}
+	exit(EXIT_SUCCESS);
+}
